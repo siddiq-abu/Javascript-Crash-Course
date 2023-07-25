f652b9694e2bb54835d5135b5ef6c503262750c6
siddiq-abu~ <abusiddiq200g3@gmail.com>
2023-07-25 12:48:05 +0000

    Javascript-Crash-Course

---
+++ app.js
@@ -0,0 +1,70 @@
+/// STRINGS ///
+/// HOW TO WRITE STRINGS ///
+
+console.log("Hello World");
+console.log("Hello" + "World");
+console.log("100");
+
+
+/// HOW TO ACCESS A SPECIFIC CHARACTER IN A STRING ///
+
+console.log("Siddiq" [0]);
+console.log("Hello World"[10]);
+
+
+/// HOW TO ACCESS THE LENGTH OF A STRING ///
+
+console.log("".length);
+console.log("Siddiq".length);
+
+/// VARIABLE ///
+
+/// WORK ///
+/// Converting Celsius to Fahrenheit ///
+
+// formula //
+// f = c * 1.8 + 32 //
+
+let celsius = 10;
+let fahrenheit = celsius * 1.8 + 32
+
+console.log(fahrenheit);
+
+
+/// EQUALITY ///
+
+// Double Equals {==} //
+
+let bool = '1' == 1;
+console.log(bool);
+
+// NOT EQUAL //
+let bol = '1' != 1;
+console.log(bol);
+
+
+// Triple Equals {===} //
+
+let srt = '1' === 1
+console.log(srt);
+
+// NOT EQUAL //
+let trs = '1' !== 1;
+console.log(trs);
+
+/// NOT EQUAL TO ///
+
+// Not Equal to //opposite //
+// under boolean //
+let str = !true ;
+console.log(str);
+
+
+// Not Equal to // opposite //
+// under boolean //
+
+let rts = !false
+console.log(rts);
+
+
+
